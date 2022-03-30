e3d3c786425ee8ff6c6419e44a77d2700a1ed7ea
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2022-03-29 21:24:37 -0300

    Create String-numeros.dart

---
+++ String-numeros.dart
@@ -0,0 +1,25 @@
+// TIPOS DE DATOS - String y números (int, double).
+
+void main(){
+  
+// - - - - - Strings - - - - - -
+  
+// var nombre = 'Tony';
+final String nombre = 'Tony';
+String apellido = 'Stark';
+  
+//   nombre = 'Peter';
+  
+  print('$nombre $apellido');
+  
+  
+// - - - - - - Números - - - - - -
+  // nota: si hago una división entre enteros, el resultado va a ser un double.
+  int empleados = 10;
+  double salario = 1856.25;
+  
+  print(empleados);
+  print(salario);
+  
+  
+}
