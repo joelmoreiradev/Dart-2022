f8437cede97960b2ff2e40b95ff8ec9e978fb163
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2022-03-31 11:51:51 -0300

    Create List.dart

---
+++ List.dart
@@ -0,0 +1,27 @@
+void main() {
+  
+  // defino un listado de enteros
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+  
+  // imprimo el arreglo completo
+  print(numeros);
+  
+  // agrego un nuevo valor al final del List
+  numeros.add(11);
+  
+  // imprimo el listado completo + el 11 
+  print(numeros);
+  
+  // imprimo el 1
+  print(numeros[0]);
+  
+  
+  
+ // - - - = generar un listado de 100 números = - - - 
+    
+  // final masNumeros = List.generate(100, (int index) => 10);
+  final masNumeros = List.generate(100, (int index) => index);
+  
+  print(masNumeros);
+  
+}
